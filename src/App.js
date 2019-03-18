773e3a2f9b04b0ca3d9caf0012e1a20ae3b67dfb
Esther Park <estherspark91@gmail.com>
2019-03-18 16:04:28 -0400

    added components folder and added SearchResult.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SearchResult from './components/SearchResult';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1 className="title">Giphy Search</h1>
+        <SearchResult />
       </div>
     );
   }
---
+++ src/components/SearchResult.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchResult extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Search Result</h1>
+      </div>
+    );
+  }
+}
