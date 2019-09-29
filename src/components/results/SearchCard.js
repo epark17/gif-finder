7bf76e1ea95af6fbbbbf0667f83b8289aa23ceb0
Esther Park <estherspark91@gmail.com>
2019-09-29 03:19:27 -0400

    updated SearchCard component

--- src/components/results/SearchCard.js
+++ src/components/results/SearchCard.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const SearchCard = props => {
-  const title = props.gif.title;
-  const fixedHeightImageURL = props.gif.images.fixed_height.url;
+const SearchCard = ({ gif }) => {
+  const title = gif.title;
+  const fixedHeightImageURL = gif.images.fixed_height.url;
 
   return (
     <React.Fragment>
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
+        {/* might get ride of card title */}
+        {/* <h5 className="card-title">{title}</h5> */}
+        <img
+          className="card-img-top img-fluid"
+          src={fixedHeightImageURL}
+          alt={title}
+        />
       </div>
-      <img
-        className="card-img-top img-fluid"
-        src={fixedHeightImageURL}
-        alt={title}
-      />
     </React.Fragment>
   );
 };
