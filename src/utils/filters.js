c2d823ad7085ac513d845daaaf77f295121d651f
Esther Park <estherspark91@gmail.com>
2019-09-29 01:00:38 -0400

    changes in filters.js

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,4 +1,4 @@
-//sorts the results from newest import date to oldest import date
+// sorts the results from newest import date to oldest import date
 export const getSortedByDate = gifs => {
   return gifs.sort((a, b) => {
     const dateA = new Date(a.import_datetime);
@@ -7,7 +7,7 @@ export const getSortedByDate = gifs => {
   });
 };
 
-//filters the results based on rating
+// filters the results based on rating
 export const getFilteredByRating = (ratingType, gifs) => {
   return ratingType !== null
     ? gifs.filter(gif => gif.rating === ratingType)
