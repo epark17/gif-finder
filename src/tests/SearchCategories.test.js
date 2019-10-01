6b465a6229fba63235de01862b7eeb5a3c251aab
Esther Park <estherspark91@gmail.com>
2019-10-01 16:22:02 -0400

    fixed SearchCategories Test, must work on tests

--- src/tests/SearchCategories.test.js
+++ src/tests/SearchCategories.test.js
@@ -4,26 +4,26 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import SearchCategories from '../components/searchResults/SearchCategories';
+import SearchCategories from '../components/results/SearchCategories';
 
 configure({ adapter: new Adapter() });
 
 describe('<SearchCategories />', () => {
   it('renders one button', () => {
     const buttons = shallow(<SearchCategories />);
-    expect(buttons.find('button').length).toEqual(1);
+    expect(buttons.find('button').length).toEqual(5);
   });
 
   it('each button invokes its respective onClick fn passed down as props', () => {
     const mockCallBackSortByDate = jest.fn();
     // const mockCallBackFilterByRatings = jest.fn();
-    // const mockCallBackClearFilter = jest.fn();
+    const mockCallBackClearFilter = jest.fn();
 
     const buttons = shallow(
       <SearchCategories
         onSortByDate={mockCallBackSortByDate}
         // onFilterByRatings={mockCallBackFilterByRatings}
-        // onClearFilter={mockCallBackClearFilter}
+        onClearFilter={mockCallBackClearFilter}
       >
         FakeButtons
       </SearchCategories>
@@ -33,10 +33,10 @@ describe('<SearchCategories />', () => {
     expect(mockCallBackSortByDate.mock.calls.length).toEqual(1);
 
     // buttons.find('#rating-btn').simulate('click');
-    // buttons.find('#g-Btn').simulate('click');
-    // expect(mockCallBackFilter.mock.calls.length).toEqual(2);
+    // buttons.find('#rated-g-btn').simulate('click');
+    // expect(mockCallBackFilterByRatings.mock.calls.length).toEqual(2);
 
-    // buttons.find('#clear-btn').simulate('click');
-    // expect(mockCallBackClear.mock.calls.length).toEqual(1);
+    buttons.find('#clear-btn').simulate('click');
+    expect(mockCallBackClearFilter.mock.calls.length).toEqual(1);
   });
 });
