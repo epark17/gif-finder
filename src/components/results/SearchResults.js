2a70f766a7b243a6994b519fa5db338c0b7e162b
Esther Park <estherspark91@gmail.com>
2019-09-30 23:29:35 -0400

    added Spinner and logic in SearchResults

--- src/components/results/SearchResults.js
+++ src/components/results/SearchResults.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SearchCategories from './SearchCategories';
 import SearchCard from './SearchCard';
-// import Spinner from '../layout/Spinner';
+import Spinner from '../layout/Spinner';
 import { getFilteredByRating, getSortedByDate } from '../../utils/filters';
 
 const SearchResult = ({
@@ -26,46 +26,42 @@ const SearchResult = ({
       />
       <br />
 
-      {
-        // if (gifs === undefined || !gifs.length) {
-        //   return <Spinner />
-        // } else {
-        //   // the code below - the gif results
-        // }
-      }
-
-      <div className="row justify-content-between">
-        {(isDataSorting &&
-          getSortedByDate(renderedGifs).map(gif => (
-            <div className="col-md-3">
-              <div className="m-2 card bg-transparent" key={gif.id}>
-                <SearchCard gif={gif} />
-              </div>
-            </div>
-          ))) ||
-          (ratingType &&
-            (getFilteredByRating(ratingType, renderedGifs).length !== 0 ? (
-              getFilteredByRating(ratingType, renderedGifs).map(gif => (
-                <div className="col-md-3">
-                  <div className="m-2 card bg-transparent" key={gif.id}>
-                    <SearchCard gif={gif} />
-                  </div>
+      {gifs === undefined || !gifs.length ? (
+        <Spinner />
+      ) : (
+        <div className="row justify-content-between">
+          {(isDataSorting &&
+            getSortedByDate(renderedGifs).map(gif => (
+              <div className="col-md-3">
+                <div className="m-2 card bg-transparent" key={gif.id}>
+                  <SearchCard gif={gif} />
                 </div>
-              ))
-            ) : (
-              <p className="searchNotFound">
-                Oops! None of the search results match this rating.
-              </p>
-            ))) ||
-          gifs.map(gif => (
-            // col-3  col-lg-3
-            <div className="col-md-3">
-              <div className="m-2 card bg-transparent" key={gif.id}>
-                <SearchCard gif={gif} />
               </div>
-            </div>
-          ))}
-      </div>
+            ))) ||
+            (ratingType &&
+              (getFilteredByRating(ratingType, renderedGifs).length !== 0 ? (
+                getFilteredByRating(ratingType, renderedGifs).map(gif => (
+                  <div className="col-md-3">
+                    <div className="m-2 card bg-transparent" key={gif.id}>
+                      <SearchCard gif={gif} />
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p className="searchNotFound">
+                  Oops! None of the search results match this rating.
+                </p>
+              ))) ||
+            gifs.map(gif => (
+              // col-3  col-lg-3
+              <div className="col-md-3">
+                <div className="m-2 card bg-transparent" key={gif.id}>
+                  <SearchCard gif={gif} />
+                </div>
+              </div>
+            ))}
+        </div>
+      )}
     </React.Fragment>
   );
 };
