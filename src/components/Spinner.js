b040f5c17ed1ece24417efff2c88ddfce1d02a27
Esther Park <estherspark91@gmail.com>
2019-10-02 02:59:43 -0400

    changed div in Spinner.js to React Fragments

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,12 +3,12 @@ import spinner from './spinner.gif';
 
 export default () => {
   return (
-    <div>
+    <React.Fragment>
       <img
         src={spinner}
         alt="Loading..."
         style={{ width: '200px', margin: '40px auto', display: 'block' }}
       />
-    </div>
+    </React.Fragment>
   );
 };
